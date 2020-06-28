c56aab8322e7d7f372ff109cf517ef8455305825
uday.khandeka <uday.khandeka@scikey.ai>
2020-06-29 00:07:05 +0530

    chore: intial commit by avb786

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,18 @@
 export const environment = {
-  production: true
+  production: true,
+  taskServiceManagement: {
+    getAllList: '/lists/:userId',
+    getAllTaskByListId: '/list/:listId/:userId/tasks',
+    createList: '/create/list/:userId',
+    deleteList: '/list/:listId/:userId',
+    createTask: '/list/:listId/tasks',
+    updateTask: '/list/:listId/tasks/:taskId',
+    deleteATask: '/list/:listId/tasks/:taskId'
+  },
+  UserManagement: {
+    userSignin: '/signin' ,
+    userSignOut: '/signout',
+    getUserDetail: '/user/:userId',
+    userSignUp: '/signup'
+  }
 };
